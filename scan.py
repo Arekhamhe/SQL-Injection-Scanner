7142d3d6df82ac864958c2aa8a1ed1bb1ec7064f
Samuel Adole <cyberorgnizm@gmail.com>
2022-11-19 22:30:47 +0100

    Create flask app for SQL injection scanner

--- scan.py
+++ scan.py
@@ -1,7 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
-from urllib.parse import urljoin
 
 s = requests.Session()
 s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
@@ -12,7 +10,6 @@ def get_forms(url):
     return soup.find_all("form")
 
 def form_details(form):
-    detailsOfForm = {}
     action = form.attrs.get("action")
     method = form.attrs.get("method", "get")
     inputs = []
@@ -26,29 +23,22 @@ def form_details(form):
             "name" : input_name,
             "value" : input_value,
         })
-        
-    detailsOfForm['action'] = action
-    detailsOfForm['method'] = method
-    detailsOfForm['inputs'] = inputs
-    return detailsOfForm
+
+    return {'action': action, 'method': method, 'inputs': inputs}
 
 def vulnerable(response):
     errors = {"quoted string not properly terminated", 
               "unclosed quotation mark after the charachter string",
               "you have an error in you SQL syntax" 
              }
-    for error in errors:
-        if error in response.content.decode().lower():
-            return True
-    return False
+    return any(error in response.content.decode().lower() for error in errors)
 
 def sql_injection_scan(url):
     forms = get_forms(url)
-    print(f"[+] Detected {len(forms)} forms on {url}.")
-    
+    messages = {f"[+] Detected {len(forms)} forms on {url}."}
     for form in forms:
         details = form_details(form)
-        
+
         for i in "\"'":
             data = {}
             for input_tag in details["inputs"]:
@@ -56,7 +46,7 @@ def sql_injection_scan(url):
                     data[input_tag['name']] = input_tag["value"] + i
                 elif input_tag["type"] != "submit":
                     data[input_tag['name']] = f"test{i}"
-    
+
             print(url)
             form_details(form)
 
@@ -65,10 +55,11 @@ def sql_injection_scan(url):
             elif details["method"] == "get":
                 res = s.get(url, params=data)
             if vulnerable(res):
-                print("SQL injection attack vulnerability in link: ", url )
+                messages.add(f"SQL injection attack vulnerability in link: {url}")
             else:
-                print("No SQL injection attack vulnerability detected")
+                messages.add("No SQL injection attack vulnerability detected")
                 break
+    return messages
 
 if __name__ == "__main__":
     urlToBeChecked = "http://olympicology.com/events/madrid-ch2/"
